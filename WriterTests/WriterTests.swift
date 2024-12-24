f85b4aa1a4dc6027fb248d55c4cc91f2c17ff038
Max Kulish <maxkulish@intel471.com>
2024-12-24 09:28:05 +0100

    Initial Commit

---
+++ WriterTests/WriterTests.swift
@@ -0,0 +1,17 @@
+//
+//  WriterTests.swift
+//  WriterTests
+//
+//  Created by Max Kul on 24.12.2024.
+//
+
+import Testing
+@testable import Writer
+
+struct WriterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
